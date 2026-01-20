195722802543b423ee55a4d3262aeca59b94e2f4
Mamoru Miura <mamo3gr@gmail.com>
2026-01-20 13:43:05 +0900

    feat: step 1

---
+++ 62_unique-paths/step1.py
@@ -0,0 +1,10 @@
+import math
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return int(
+            math.factorial((m - 1) + (n - 1))
+            / math.factorial(m - 1)
+            / math.factorial(n - 1)
+        )
