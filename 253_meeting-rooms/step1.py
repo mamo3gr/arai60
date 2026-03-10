cf4f1fc8a2118977d91c67183c0113559c960e89
Mamoru Miura <mamo3gr@gmail.com>
2026-03-11 06:05:33 +0900

    feat: step 1

---
+++ 253_meeting-rooms/step1.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: list[Interval]) -> bool:
+        intervals_sorted = sorted(intervals, key=lambda x: x.start)
+        for i in range(len(intervals_sorted) - 1):
+            if intervals_sorted[i].end > intervals_sorted[i + 1].start:
+                return False
+
+        return True
