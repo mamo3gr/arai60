abeba0870597cb0f1564af61ff12c24550864feb
Mamoru Miura <mamo3gr@gmail.com>
2026-03-11 05:24:58 +0900

    fix: correct return code

--- 776_split-bst/step2.py
+++ 776_split-bst/step2.py
@@ -1,1 +1,15 @@
-def splitBST(root: TreeNode, val: int) -> tuple[TreeNode | None, TreeNode | None]:    def helper(node: TreeNode | None) -> tuple[TreeNode | None, TreeNode | None]:        if node is None:            return None, None        if node.val <= val:            child_left, child_right = helper(node.right)            node.right = child_left            return node, child_right        else:            child_left, child_right = helper(node.left)            node.left = child_right            return child_left, node    return helper(root)
+def splitBST(root: TreeNode, val: int) -> tuple[TreeNode | None, TreeNode | None]:
+    def helper(node: TreeNode | None) -> tuple[TreeNode | None, TreeNode | None]:
+        if node is None:
+            return None, None
+
+        if node.val <= val:
+            child_left, child_right = helper(node.right)
+            node.right = child_left
+            return node, child_right
+        else:
+            child_left, child_right = helper(node.left)
+            node.left = child_right
+            return child_left, node
+
+    return helper(root)
